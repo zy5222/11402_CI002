84c63cf83ca10e1d8995f9032417f3416edbac44
王芷芸 <s1131547@mail.yzu.edu.tw>
2026-06-07 23:09:51 +0800

    Implement B2-Sequence validation in 11063.cpp

--- Assignment/Assignment II/On-Site_Examinations/Week7_13_UVa11063/src/11063.cpp
+++ Assignment/Assignment II/On-Site_Examinations/Week7_13_UVa11063/src/11063.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+#include<set>
+using namespace std;
 
+int main() {
+	int n;
+	int t = 0;
+	while (cin >> n) {
+		bool isB2 = true;
+		int b[10000];
+		for (int i = 0; i < n; i++) {
+			cin >> b[i];
+			if (b[i] < 1) isB2 = false;
+		}
+			
+		for (int i = 0; i < n - 1; i++) {
+			if (b[i] >= b[i + 1])
+				isB2 = false;
+		}
+		set<int> sums;
+		for (int i = 0; i < n; i++) {
+			for (int j = i; j < n; j++) {
+				int num = b[i] + b[j];
+				if (sums.count(num))
+					isB2 = false;
+				sums.insert(num);
+			}
+		}
+		cout << "Case #" << ++t << ": ";
+		if (isB2)
+			cout << "It is a B2-Sequence.\n";
+		else
+			cout << "It is not a B2-Sequence.\n";
+
+		cout << "\n";
+	}
+	return 0;
+}
